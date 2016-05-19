ef6e181a46ece2f97026f6c5e9481eede32f825e
lv <lv@lvdeMacBook-Pro.local>
2016-05-19 16:27:24 +0800

    Hello gitHub
    gitHub

---
+++ HelloGitHub/HelloGitHub/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HelloGitHub
+//
+//  Created by lv on 16/5/19.
+//  Copyright © 2016年 lv. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"Hello GitHub");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
